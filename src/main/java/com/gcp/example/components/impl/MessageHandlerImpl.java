eddf85a5da11347d79e81ccf839a9260be46d110
Vitalii Vorobii <41230006+vorobii-vitalii@users.noreply.github.com>
2020-11-27 16:11:29 +0200

    Create MessageHandlerImpl.java

---
+++ src/main/java/com/gcp/example/components/impl/MessageHandlerImpl.java
@@ -0,0 +1,36 @@
+package com.gcp.example.components.impl;
+
+import com.gcp.example.components.MessageProcessor;
+import com.gcp.example.exceptions.MessageProcessingException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
+import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class MessageHandlerImpl implements MessageHandler {
+    private final MessageProcessor messageProcessor;
+
+    @ServiceActivator(inputChannel = "pubsubInputChannel")
+    @Override
+    public void handleMessage(Message<?> message) throws MessagingException {
+        final String payload = new String((byte[]) message.getPayload());
+        messageProcessor.process(payload);
+        answerBack(message);
+    }
+
+    private void answerBack(Message<?> message) {
+        BasicAcknowledgeablePubsubMessage originalMessage =
+                message.getHeaders().get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage.class);
+        if (originalMessage != null)
+            originalMessage.ack();
+    }
+
+}
