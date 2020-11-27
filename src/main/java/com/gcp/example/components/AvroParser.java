95c6f02da00b58ef2629f4b8fa51d2cffa81c8ab
Vitalii Vorobii <41230006+vorobii-vitalii@users.noreply.github.com>
2020-11-27 16:11:13 +0200

    Create AvroParser.java

---
+++ src/main/java/com/gcp/example/components/AvroParser.java
@@ -0,0 +1,21 @@
+package com.gcp.example.components;
+
+import org.apache.avro.file.DataFileStream;
+import org.apache.avro.io.DatumReader;
+import org.springframework.cloud.gcp.bigquery.core.BigQueryTemplate;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public abstract class AvroParser<T> {
+    protected abstract DatumReader<T> getDatumReader();
+
+    public T retrieve(InputStream inputStream) throws IOException {
+        DataFileStream<T> fileStream = new DataFileStream<>(inputStream, getDatumReader());
+        T t = null;
+        if (fileStream.hasNext()) {
+            t = fileStream.next(t);
+        }
+        return t;
+    }
+}
