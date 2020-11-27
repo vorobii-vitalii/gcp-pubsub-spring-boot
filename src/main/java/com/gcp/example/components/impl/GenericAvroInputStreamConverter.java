0c880b71dd5b41db555807f8d0fe9b3d74d34ec3
Vitalii Vorobii <41230006+vorobii-vitalii@users.noreply.github.com>
2020-11-27 16:11:23 +0200

    Create GenericAvroInputStreamConverter.java

---
+++ src/main/java/com/gcp/example/components/impl/GenericAvroInputStreamConverter.java
@@ -0,0 +1,30 @@
+package com.gcp.example.components.impl;
+
+import com.gcp.example.components.AvroInputStreamConverter;
+import example.gcp.Client;
+import example.gcp.ClientMainInfo;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.avro.specific.SpecificRecord;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class GenericAvroInputStreamConverter<T extends SpecificRecord> implements AvroInputStreamConverter<T> {
+    @Override
+    public InputStream convert(T t) throws IOException {
+        DatumWriter<T> datumWriter = new SpecificDatumWriter<T>();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        DataFileWriter<T> dataFileWriter = new DataFileWriter<>(datumWriter);
+        dataFileWriter.create(t.getSchema(), outputStream);
+        dataFileWriter.append(t);
+        dataFileWriter.close();
+
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+}
