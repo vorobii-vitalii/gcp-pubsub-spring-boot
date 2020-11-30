7f4b669267b48938e2f9e25ce4e28bb36fde9b97
Vitalii Vorobii <41230006+vorobii-vitalii@users.noreply.github.com>
2020-11-30 21:04:31 +0200

    Update GCPConfig.java

--- src/main/java/com/gcp/example/config/GCPConfig.java
+++ src/main/java/com/gcp/example/config/GCPConfig.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.gcp.pubsub.integration.AckMode;
 import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
@@ -28,22 +29,22 @@ import java.time.Duration;
 @Slf4j
 public class GCPConfig {
 
-    @Bean
-    public PubSubInboundChannelAdapter messageChannelAdapter(
-            @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
-            PubSubTemplate pubSubTemplate) {
-        PubSubInboundChannelAdapter adapter =
-                new PubSubInboundChannelAdapter(pubSubTemplate, "AVRO_PROCESSOR");
-        adapter.setOutputChannel(inputChannel);
-        adapter.setAckMode(AckMode.MANUAL);
-
-        return adapter;
-    }
-
-    @Bean
-    public MessageChannel pubsubInputChannel() {
-        return new DirectChannel();
-    }
+//    @Bean
+//    public PubSubInboundChannelAdapter messageChannelAdapter(
+//            @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
+//            PubSubTemplate pubSubTemplate) {
+//        PubSubInboundChannelAdapter adapter =
+//                new PubSubInboundChannelAdapter(pubSubTemplate, "AVRO_PROCESSOR");
+//        adapter.setOutputChannel(inputChannel);
+//        adapter.setAckMode(AckMode.MANUAL);
+//
+//        return adapter;
+//    }
+//
+//    @Bean
+//    public MessageChannel pubsubInputChannel() {
+//        return new DirectChannel();
+//    }
 
     @Bean
     public DirectChannel bigQueryWriteDataChannel() {
@@ -67,6 +68,7 @@ public class GCPConfig {
     }
 
     @Bean
+    @Primary
     public GatewayProxyFactoryBean gatewayProxyFactoryBean() {
         GatewayProxyFactoryBean factoryBean = new GatewayProxyFactoryBean(BigQueryClientAvroFileGateway.class);
         factoryBean.setDefaultRequestChannel(bigQueryWriteDataChannel());
