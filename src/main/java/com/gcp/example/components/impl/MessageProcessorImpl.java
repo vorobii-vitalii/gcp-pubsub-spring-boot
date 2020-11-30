70f574a39f1c23060a850193494290b38bf322ff
Vitalii Vorobii <41230006+vorobii-vitalii@users.noreply.github.com>
2020-11-30 21:04:19 +0200

    Update MessageProcessorImpl.java

--- src/main/java/com/gcp/example/components/impl/MessageProcessorImpl.java
+++ src/main/java/com/gcp/example/components/impl/MessageProcessorImpl.java
@@ -1,14 +1,12 @@
 package com.gcp.example.components.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gcp.example.components.AvroInputStreamConverter;
 import com.gcp.example.components.AvroParser;
 import com.gcp.example.components.Mapper;
 import com.gcp.example.config.GCPConfig;
 import com.gcp.example.model.Client;
 import com.gcp.example.model.ClientMainInfo;
-import com.gcp.example.model.MessageInfo;
+import com.gcp.example.model.Body;
 import com.gcp.example.components.MessageProcessor;
 import com.gcp.example.exceptions.MessageProcessingException;
 import com.google.cloud.bigquery.Job;
@@ -31,7 +29,6 @@ import java.util.concurrent.CompletableFuture;
 public class MessageProcessorImpl implements MessageProcessor {
     private static final String GOOGLE_STORAGE_PREFIX = "gs://";
 
-    private final ObjectMapper objMapper;
     private final Storage storage;
     private final AvroParser<Client> clientAvroParser;
     private final Mapper<Client, ClientMainInfo> clientMapper;
@@ -39,15 +36,15 @@ public class MessageProcessorImpl implements MessageProcessor {
     private final GCPConfig.BigQueryClientAvroFileGateway bigQueryClientAvroFileGateway;
 
     @Override
-    public void process(String payload) {
+    public void process(Body.Message.MessageAttributes attributes) {
+        final String resourceUrl = formGoogleStorageLink(attributes);
+        final GoogleStorageResource resource = new GoogleStorageResource(storage, resourceUrl);
+        process(resource);
+    }
 
+    public void process(GoogleStorageResource gsResource) {
         try {
-            final MessageInfo messageInfo = objMapper.readValue(payload, MessageInfo.class);
-            log.info("Received message: " + messageInfo);
-
-            final String resourceUrl = formGoogleStorageLink(messageInfo);
-            final Resource resource = new GoogleStorageResource(storage, resourceUrl);
-            final InputStream inputStream = resource.getInputStream();
+            final InputStream inputStream = gsResource.getInputStream();
 
             final Client client = clientAvroParser.retrieve(inputStream);
             final ClientMainInfo clientMainInfo = clientMapper.map(client);
@@ -56,28 +53,24 @@ public class MessageProcessorImpl implements MessageProcessor {
             final InputStream clientMainInfoStream = avroInputStreamConverter.convert(clientMainInfo);
 
             final SettableListenableFuture<Job> future1 =
-                bigQueryClientAvroFileGateway.writeToBigQueryTable(clientStream, "Client");
+                    bigQueryClientAvroFileGateway.writeToBigQueryTable(clientStream, "Client");
 
             final SettableListenableFuture<Job> future2 =
-                bigQueryClientAvroFileGateway.writeToBigQueryTable(clientMainInfoStream, "Client_Main_Info");
+                    bigQueryClientAvroFileGateway.writeToBigQueryTable(clientMainInfoStream, "Client_Main_Info");
 
             CompletableFuture.allOf(future1.completable(), future2.completable()).join();
             log.info("Data has been successfully inserted in a database");
         }
-        catch (JsonProcessingException | BigQueryException e) {
-            log.error("Message processing error or error while executing query");
-            e.printStackTrace();
-        }
-        catch (IOException  ignored) {
-            log.error("Unknown error occurred during processing");
+        catch ( BigQueryException | IOException e) {
+            log.error("Message processing has failed");
             throw new MessageProcessingException();
         }
     }
 
-    private String formGoogleStorageLink(MessageInfo messageInfo) {
+    public String formGoogleStorageLink(Body.Message.MessageAttributes attributes) {
         return  GOOGLE_STORAGE_PREFIX +
-                messageInfo.getBucket() +
+                attributes.getBucketId() +
                 "/" +
-                messageInfo.getName();
+                attributes.getObjectId();
     }
 }
