65ecb038161f4612ae6f001ac7b40cb0dbbdc59f
Vitalii Vorobii <41230006+vorobii-vitalii@users.noreply.github.com>
2020-12-03 01:13:01 +0200

    Create MappingException.java

---
+++ src/main/java/com/gcp/example/exceptions/MappingException.java
@@ -0,0 +1,14 @@
+package com.gcp.example.exceptions;
+
+public class MappingException extends RuntimeException {
+    public MappingException() {
+    }
+
+    public MappingException(String message) {
+        super(message);
+    }
+
+    public MappingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
