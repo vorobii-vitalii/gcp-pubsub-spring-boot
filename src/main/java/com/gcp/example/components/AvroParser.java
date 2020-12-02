823ce8c336be563278a0f16033ddefd3988c7983
Vitalii Vorobii <41230006+vorobii-vitalii@users.noreply.github.com>
2020-12-03 01:12:55 +0200

    Fixed avro parsing logic

--- src/main/java/com/gcp/example/components/AvroParser.java
+++ src/main/java/com/gcp/example/components/AvroParser.java
@@ -5,17 +5,21 @@ import org.apache.avro.io.DatumReader;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public abstract class AvroParser<T> {
     protected abstract DatumReader<T> getDatumReader();
 
-    public T retrieve(InputStream inputStream) throws IOException {
+    public List<T> retrieve(InputStream inputStream) throws IOException {
         DataFileStream<T> fileStream = new DataFileStream<>(inputStream, getDatumReader());
+        final List<T> result = new ArrayList<>();
         T t = null;
         if (fileStream.hasNext()) {
             t = fileStream.next(t);
+            result.add(t);
         }
-        return t;
+        return result;
     }
 }
