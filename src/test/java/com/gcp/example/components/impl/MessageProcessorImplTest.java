cde412f61597e26a2b8be12a676c593f1e3538b5
Vitalii Vorobii <41230006+vorobii-vitalii@users.noreply.github.com>
2020-12-03 01:13:52 +0200

    Update MessageProcessorImplTest.java

--- src/test/java/com/gcp/example/components/impl/MessageProcessorImplTest.java
+++ src/test/java/com/gcp/example/components/impl/MessageProcessorImplTest.java
@@ -1,13 +1,12 @@
 package com.gcp.example.components.impl;
 
-import com.gcp.example.components.AvroInputStreamConverter;
 import com.gcp.example.components.AvroParser;
-import com.gcp.example.config.GCPConfig;
 import com.gcp.example.exceptions.MessageProcessingException;
-import com.gcp.example.model.Body;
 import com.gcp.example.model.Client;
+import com.gcp.example.model.GSMessage;
+import com.gcp.example.repository.GoogleStorageRepository;
+import com.gcp.example.services.impl.MessageProcessorImpl;
 import com.google.cloud.storage.Storage;
-import org.apache.avro.specific.SpecificRecord;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +19,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class MessageProcessorImplTest {
-    private Body.Message.MessageAttributes messageAttributes;
+    private GSMessage gsMessage;
 
     @MockBean
     private Storage mockStorage;
@@ -36,35 +34,31 @@ class MessageProcessorImplTest {
     private MessageProcessorImpl messageProcessor;
 
     @MockBean
-    private AvroInputStreamConverter<SpecificRecord> avroInputStreamConverter;
-
-    @MockBean
     private AvroParser<Client> clientAvroParser;
 
-    @MockBean
-    private GCPConfig.BigQueryClientAvroFileGateway bigQueryClientAvroFileGateway;
+    @Autowired
+    private GoogleStorageRepository googleStorageRepository;
 
     @BeforeEach
     public void setUp() {
-        messageAttributes = new Body.Message.MessageAttributes();
-        messageAttributes.setBucketId("123");
-        messageAttributes.setObjectId("test.avro");
+        gsMessage = new GSMessage();
+        gsMessage.setBucketId("123");
+        gsMessage.setObjectId("test.avro");
     }
 
     @Test
     void testThrowIOExceptionWhenObjectNotFound() {
-        final String generatedLink = messageProcessor.formGoogleStorageLink(messageAttributes);
-        when(mockStorage.get(messageAttributes.getObjectId())).thenReturn(null);
+        when(mockStorage.get(gsMessage.getObjectId())).thenReturn(null);
         assertThrows(FileNotFoundException.class, () ->
-                new GoogleStorageResource(mockStorage, generatedLink).getInputStream()
+                googleStorageRepository.getResource(gsMessage).getInputStream()
         );
     }
 
     @Test
     void testThrowCustomExceptionFromMethodWhenInputStreamIsCorrupted() {
-        when(mockStorage.get(messageAttributes.getObjectId())).thenReturn(null);
+        when(mockStorage.get(gsMessage.getObjectId())).thenReturn(null);
         assertThrows(MessageProcessingException.class, () ->
-                messageProcessor.process(messageAttributes)
+                messageProcessor.process(gsMessage)
         );
     }
 
@@ -78,14 +72,8 @@ class MessageProcessorImplTest {
         when(clientAvroParser.retrieve(expectedInputStream)).thenThrow(IOException.class);
 
         assertThrows(MessageProcessingException.class, () ->
-                messageProcessor.process(messageAttributes)
+                messageProcessor.process(gsMessage)
         );
-    }
-
-    @Test
-    void testFormGoogleStorageURI() {
-        final String generatedLink = messageProcessor.formGoogleStorageLink(messageAttributes);
-        assertEquals("gs://123/test.avro", generatedLink);
     }
 
 }
